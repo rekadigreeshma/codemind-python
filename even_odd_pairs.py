630c8b255b03f25c390b1a3ebead8e8cce849d7a
rekadigreeshma <98876161+rekadigreeshma@users.noreply.github.com>
2023-06-06 09:58:24 +0530

    n = int(input()) arr = list(map(int,input().split())) even = [i for i in arr if i % 2 == 0] odd = [i for i in arr if i % 2 == 1] e,o = 0,0 while e < len(even) or o < len(odd):     if e < len(even):         print(even[e],end = ' ')         e += 1     if o<len(odd):         print(odd[o],end = ' ')         o +=1 if n % 2 !=0:     print(0)

---
+++ even_odd_pairs.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int,input().split()))
+even = [i for i in arr if i % 2 == 0]
+odd = [i for i in arr if i % 2 == 1]
+e,o = 0,0
+while e < len(even) or o < len(odd):
+    if e < len(even):
+        print(even[e],end = ' ')
+        e += 1
+    if o<len(odd):
+        print(odd[o],end = ' ')
+        o +=1
+if n % 2 !=0:
+    print(0)
